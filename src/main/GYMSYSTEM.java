3a60fe1d3df72bec1332bc9e72684881533bfa7f
Alex <126062825+Galas25@users.noreply.github.com>
2024-06-16 13:01:50 +0800

    Structural Changes 16/06/24

--- src/main/GYMSYSTEM.java
+++ src/main/GYMSYSTEM.java
@@ -1,36 +1,138 @@
 package main;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 public class GYMSYSTEM {
   String username = "admin";
   String password = "test123";
 
   public static void main(String[] args) {
     GYMSYSTEM g1 = new GYMSYSTEM();
+    login();
     //       g1.login();
     //       g1.signUp();
-    dashboard dash = new dashboard();
+//    dashboard dash = new dashboard();
 
   }
 
-  public void login() {
-    JFrame f1 = new JFrame();
-    f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f1.setSize(500, 540);
-    f1.setTitle("Login");
-    f1.setLocationRelativeTo(null);
-    f1.toFront();
-    f1.setVisible(true);
+  public static void login() {
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JLabel label = new JLabel();
+    JButton login = new JButton();
+    JButton signup = new JButton();
+    JTextField username = new JTextField();
+    JLabel user = new JLabel("Username");
+    JPasswordField password = new JPasswordField();
+    JLabel user_password = new JLabel("Password");
+    user.setBounds(150, 60, 200, 40);
+    username.setBounds(150, 100, 200, 40);
+    user_password.setBounds(150, 150, 200, 40);
+    password.setBounds(150, 200, 200, 40);
+    frame.setTitle("Gym Management System");
+    frame.setLayout(null);
+    frame.add(username);
+    frame.add(panel);
+    frame.add(user);
+    frame.add(user_password);
+    frame.add(password);
+    label.setText("Welcome to our Gym ");
+    label.setHorizontalTextPosition(JLabel.CENTER);
+    label.setVerticalTextPosition(JLabel.TOP);
+    label.setBounds(0, 0, 200, 100);
+    label.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+    panel.setBounds(0, 0, 500, 50);
+    panel.add(label);
+    login.setText("Login");
+    login.setBounds(150, 250, 200, 50);
+    frame.add(login);
+
+    signup.setText("Sign up");
+    signup.setBounds(150, 320, 200, 50);
+    frame.add(signup);
+    signup.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        frame.dispose();
+        signUp();
+
+      }
+    });
+    login.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        new dashboard();
+
+      }
+    });
+
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBounds(450, 180, 500, 540);
+    frame.setLocationRelativeTo(null);
+    frame.toFront();
   }
 
-  public void signUp() {
+  public static void signUp() {
+    JFrame frame = new JFrame();
+    JLabel label = new JLabel();
 
-    JFrame f1 = new JFrame();
-    f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f1.setSize(500, 540);
-    f1.setTitle("Sign-up");
-    f1.setLocationRelativeTo(null);
-    f1.toFront();
-    f1.setVisible(true);
+    
+    frame.setTitle("Sign Up");
+    frame.setLocationRelativeTo(null);
+    frame.setLayout(null);
+
+    JButton register = new JButton("Register");
+
+    JPasswordField password = new JPasswordField();
+
+    JLabel user = new JLabel("Username");
+
+    JLabel user_password = new JLabel("Password");
+
+    JTextField username = new JTextField();
+
+    //  user's username
+    user.setBounds(150, 60, 200, 40);
+    username.setBounds(150, 100, 200, 40);
+
+    //user's password
+    user_password.setBounds(150, 150, 200, 40);
+    password.setBounds(150, 200, 200, 40);
+
+    register.setBounds(150, 250, 200, 40);
+
+    register.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        frame.dispose();
+        login();
+      }
+    });
+    // Add WindowListener to trigger function on close
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                login();
+            }
+        });
+    frame.add(register);
+    frame.add(user);
+    frame.add(username);
+    frame.add(user_password);
+    frame.add(password);
+    frame.setBounds(450, 180, 500, 540);
+    frame.setLocationRelativeTo(null);
+    frame.toFront();
+    frame.setVisible(true);
 
   }
 
