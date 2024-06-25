836049b594884c13d25607b4e99a6ab101f2c937
Deybidd <163238128+Deybidd@uses.noreply.github.com>
2024-06-25 16:14:15 +0800

    David Branch

--- src/main/userPage.java
+++ src/main/userPage.java
@@ -17,6 +17,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 
@@ -75,8 +76,17 @@ public class userPage extends gym{
 
         JLabel nameLbl = new JLabel(memberUsername, JLabel.CENTER);
         nameLbl.setBounds(50, 120, 100, 100);
-        nameLbl.setFont(new java.awt.Font("Inter SemiBold", Font.BOLD, 20));
+        nameLbl.setFont(new java.awt.Font("Inter SemiBold", Font.BOLD, 30));
         p2.add(nameLbl);
+
+        JButton checkIn = new JButton("Check-In");
+        checkIn.setBounds(50, 200, 100, 40);
+        p2.add(checkIn);
+        
+        JButton checkOut = new JButton("Check-Out");
+        checkOut.setBounds(50, 250, 100, 40);
+        p2.add(checkOut);
+
 
         JLabel logout = new JLabel("LOG-OUT", JLabel.CENTER);
         logout.setForeground(Color.white);
@@ -105,7 +115,7 @@ public class userPage extends gym{
         logout.setIcon(logoutIcon);
         p2.add(logout);
     
-        JLabel title = new JLabel("Gym Management System");
+        JLabel title = new JLabel("GYM NAME");
         title.setBounds(20, -15, 500, 100);
         title.setForeground(Color.white);
         title.setFont(new java.awt.Font("Inter Medium", Font.PLAIN, 20));
@@ -125,32 +135,104 @@ public class userPage extends gym{
             mainPanel.setBounds(20, 20, 742, 200);
             mainPanel.setBackground(Color.WHITE);
             mainPanel.setLayout(null);
+
+            JLabel dumbbell = new JLabel();
+            dumbbell.setBounds(490, 0, 307, 307);
+
+            ImageIcon scaledImg = new ImageIcon(
+                new ImageIcon(userPage.class.getResource("dumbbell.png"))
+                .getImage()
+                .getScaledInstance(307, 307, Image.SCALE_SMOOTH)
+            );
+
+            dumbbell.setIcon(scaledImg);
+            mainPanel.add(dumbbell);
             
             add(mainPanel);
 
+
             JLabel welcome = new JLabel("Welcome!");
-            welcome.setBounds(20, -20, 400, 100);
-            welcome.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+            welcome.setBounds(20, -10, 400, 100);
+            welcome.setFont(new Font("Segoe UI", Font.PLAIN, 30));
             mainPanel.add(welcome);
 
-            JLabel user = new JLabel(memberUsername);
-            user.setBounds(20, 20, 400, 100);
-            user.setFont(new Font("Segoe UI", Font.BOLD, 62));
-            mainPanel.add(user);
+            // JLabel user = new JLabel("(Last Name)" + ", " + memberUsername);
+            JLabel username = new JLabel("(Last Name)");//Change "Last Name"
+            username.setBounds(20, 40, 400, 100);
+            username.setFont(new Font("Segoe UI", Font.BOLD, 55));
+            mainPanel.add(username);
+
+            JLabel firstname = new JLabel(memberUsername);
+            firstname.setBounds(20, 100, 400, 100);
+            firstname.setFont(new Font("Segoe UI", Font.BOLD, 45));
+            mainPanel.add(firstname);
 
             JPanel bottomLeft = new JPanel();
             bottomLeft.setBounds(20, 240, 300, 230);
             bottomLeft.setBackground(Color.WHITE);
             bottomLeft.setLayout(null);
 
+            JLabel subscription = new JLabel("Gym Subscription Status: ");
+            subscription.setBounds(15, -20, 300, 90);
+            subscription.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+            bottomLeft.add(subscription);
+
+            JLabel status = new JLabel("Status: ");
+            status.setBounds(15, 10, 300, 90);
+            status.setFont(new Font("Segoe UI", Font.BOLD, 20));
+            bottomLeft.add(status);
+
+            JLabel currentStatus = new JLabel("Current Status");
+            currentStatus.setBounds(85, 10, 300, 90);
+            currentStatus.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+            bottomLeft.add(currentStatus);
+
+            JLabel expiresIn = new JLabel("Expires In: " + "");
+            expiresIn.setBounds(15, 40, 300, 90);
+            expiresIn.setFont(new Font("Segoe UI", Font.BOLD, 20));
+            bottomLeft.add(expiresIn);
+
+            JLabel expireDate = new JLabel("Expires Date");
+            expireDate.setBounds(120, 40, 300, 90);
+            expireDate.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+            bottomLeft.add(expireDate);
+
             add(bottomLeft);
+
 
             JPanel bottomRight = new JPanel();
             bottomRight.setBounds(340, 240, 420, 230);
             bottomRight.setBackground(Color.WHITE);
             bottomRight.setLayout(null);
 
+            JLabel userInfo = new JLabel("User Information ");
+            userInfo.setBounds(15, -20, 300, 90);
+            userInfo.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+            bottomRight.add(userInfo);
+
+            JLabel name = new JLabel("Name: ");
+            name.setBounds(15, 10, 300, 90);
+            name.setFont(new Font("Segoe UI", Font.BOLD, 20));
+            bottomRight.add(name);
+
+            JLabel fullName = new JLabel("Last Name" + ", " + memberUsername);
+            fullName.setBounds(85, 10, 300, 90);
+            fullName.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+            bottomRight.add(fullName);
+
+            JLabel contactInfo = new JLabel("Contact: ");
+            contactInfo.setBounds(15, 40, 300, 90);
+            contactInfo.setFont(new Font("Segoe UI", Font.BOLD, 20));
+            bottomRight.add(contactInfo);
+
+            JLabel contactNumber = new JLabel("Contact Number");
+            contactNumber.setBounds(100, 40, 300, 90);
+            contactNumber.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+            bottomRight.add(contactNumber);
+
             add(bottomRight);
+
+            
         }
     }
 }
